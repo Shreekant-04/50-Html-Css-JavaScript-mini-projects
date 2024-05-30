91b05ff1335d1e6336a246677acc4fb067ff5ffa
Shreekant <Shree.kant.74.sk@gmail.com>
2024-05-30 15:03:56 +0530

    Final 2

--- GithubSearch/app.js
+++ GithubSearch/app.js
@@ -1,24 +1,4 @@
 console.log("hello");
-// Replace 'your_token_here' with your personal access token
-const token = "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";
-const headers = new Headers({
-  Authorization: `token ${token}`,
-});
-
-async function checkRateLimit() {
-  try {
-    const response = await fetch("https://api.github.com/rate_limit", {
-      headers,
-    });
-    const rateLimit = await response.json();
-    const core = rateLimit.rate;
-    const resetTime = new Date(core.reset * 1000);
-  } catch (error) {
-    console.error("Error fetching rate limit:", error);
-  }
-}
-
-checkRateLimit();
 
 const nav = document.querySelectorAll("nav a");
 nav.forEach((el) => {
@@ -45,11 +25,10 @@ searchBtn.addEventListener("click", () => {
 });
 
 async function searchprofile(url, repo) {
-  checkRateLimit();
 
-  let search = await fetch(url, { headers });
+  let search = await fetch(url);
   let data = await search.json();
-  let fetchRepo = await fetch(repo, { headers });
+  let fetchRepo = await fetch(repo);
   let repos = await fetchRepo.json();
   fetchedData(search, data, repos);
 }
